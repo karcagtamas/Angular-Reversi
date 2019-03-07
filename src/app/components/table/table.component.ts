c84bdfb1f9f4a7adca426dcc5f1600247df66e61
karcag.tamas <karcagtamas@gmail.com>
2019-03-07 23:53:25 +0100

    diagonal

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -41,6 +41,7 @@ export class TableComponent implements OnInit {
     console.log(Field);
     console.log(this.checkRow(Field));
     console.log(this.checkCol(Field));
+    console.log(this.checkMainDiagonal(Field));
   }
 
   checkHasField(): boolean {
@@ -113,9 +114,136 @@ export class TableComponent implements OnInit {
     this.setCol(Field.y, startx, endx);
     return true;
   }
+
   setCol(col: number, startx: number, endx: number): void {
     for (let i = startx; i <= endx; i++) {
       this.Fields[i][col].owner = this.currentUser;
     }
+  }
+
+  // A végén ne álljanak be az eredeti érékek a többi check miatt
+
+  checkMainDiagonal(Field: Field): boolean {
+    let startx = 0;
+    let starty = 0;
+    let endx = 0;
+    let endy = 0;
+    let X = 0;
+    let Y = 0;
+    let length = 0;
+    if (Field.x === Field.y) {
+      X = 0;
+      Y = 0;
+      length = 8;
+    } else if (Field.x > Field.y) {
+      X = Field.x - Field.y - 1;
+      Y = 0;
+      length = 8 - Field.x;
+    } else if (Field.x < Field.y) {
+      X = 0;
+      Y = Field.y - Field.x - 1;
+      length = 8 - Field.y;
+    }
+    let x = X;
+    let y = Y;
+    for (let k = 0; k < length && startx === 0 && starty === 0 && endx === 0 && endy === 0; k++) {
+      if (
+        this.Fields[x][y].owner === this.currentUser &&
+        Field.x !== x &&
+        Field.y !== y &&
+        Field.x !== x - 1 &&
+        Field.y !== y - 1 &&
+        Field.x !== x + 1 &&
+        Field.y !== y + 1
+      ) {
+        startx = Field.x + Field.y > x + y ? x : Field.x;
+        starty = Field.x + Field.y > x + y ? y : Field.y;
+        endx = Field.x + Field.y < x + y ? x : Field.x;
+        endy = Field.x + Field.y < x + y ? y : Field.y;
+      }
+      x++;
+      y++;
+    }
+
+    if (startx !== 0 && starty !== 0 && endx !== 0 && endy !== 0) {
+      for (let i = startx + 1; i < endx; i++) {
+        if (this.Fields[i][Field.y].owner === 0 || this.Fields[i][Field.y].owner === this.currentUser) {
+          return false;
+        }
+      }
+      while (startx !== endx && starty !== endy) {
+        if (this.Fields[startx][starty].owner === 0 || this.Fields[startx][starty] === this.currentUser) {
+          return false;
+        }
+        startx++;
+        starty++;
+      }
+    } else {
+      return false;
+    }
+    // this.setCol(Field.y, startx, endx);
+    return true;
+  }
+
+  checkNotMainDiagonal(Field: Field): boolean {
+    let startx = 0;
+    let starty = 0;
+    let endx = 0;
+    let endy = 0;
+    let X = 0;
+    let Y = 0;
+    let length = 0;
+    if (Field.x === Field.y) {
+      X = 0;
+      Y = 0;
+      length = 8;
+    } else if (Field.x > Field.y) {
+      X = Field.x - Field.y - 1;
+      Y = 0;
+      length = 8 - Field.x;
+    } else if (Field.x < Field.y) {
+      X = 0;
+      Y = Field.y - Field.x - 1;
+      length = 8 - Field.y;
+    }
+    let x = X;
+    let y = Y;
+    for (let k = 0; k < length && startx === 0 && starty === 0 && endx === 0 && endy === 0; k++) {
+      if (
+        this.Fields[x][y].owner === this.currentUser &&
+        Field.x !== x &&
+        Field.y !== y &&
+        Field.x !== x - 1 &&
+        Field.y !== y - 1 &&
+        Field.x !== x + 1 &&
+        Field.y !== y + 1
+      ) {
+        startx = Field.x + Field.y > x + y ? x : Field.x;
+        starty = Field.x + Field.y > x + y ? y : Field.y;
+        endx = Field.x + Field.y < x + y ? x : Field.x;
+        endy = Field.x + Field.y < x + y ? y : Field.y;
+      }
+      x++;
+      y++;
+    }
+
+    if (startx !== 0 && starty !== 0 && endx !== 0 && endy !== 0) {
+      for (let i = startx + 1; i < endx; i++) {
+        if (this.Fields[i][Field.y].owner === 0 || this.Fields[i][Field.y].owner === this.currentUser) {
+          return false;
+        }
+      }
+      while (startx !== endx && starty !== endy) {
+        if (this.Fields[startx][starty].owner === 0 || this.Fields[startx][starty] === this.currentUser) {
+          return false;
+        }
+        startx++;
+        starty++;
+      }
+    } else {
+      return false;
+    }
+    // this.setCol(Field.y, startx, endx);
+    return true;
   }
 }
